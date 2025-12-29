c704ca97dbd7f2fceb1b6078862a407a5c5fa5b9
Wei Wang <ww000721@gmail.com>
2025-12-29 17:59:19 -0500

    :sparkles: Add pure-Python fallback

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 import numpy as np
 
-from afv.finite_voronoi import PhysicalParams, FiniteVoronoiSimulator
+from afv.finite_voronoi import PhysicalParams, FiniteVoronoiSimulator, _USING_ACCEL
 
 
 @pytest.fixture(scope="session")
@@ -26,6 +26,8 @@ def phys() -> PhysicalParams:
 
 @pytest.fixture(scope="session")
 def simulator(phys) -> FiniteVoronoiSimulator:
+    assert _USING_ACCEL, "Accelerated backend is not in use."
+
     # Initialize the Voronoi simulator with the defined parameters
     pts = np.array([[0.0, 0.0]])
     return FiniteVoronoiSimulator(pts, phys)
