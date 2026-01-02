67aac93ca6c251bba90e72ae9a33708589576de2
Wei Wang <ww000721@gmail.com>
2026-01-01 20:03:43 -0500

    :memo: Update version info

--- afv/__init__.py
+++ afv/__init__.py
@@ -2,7 +2,7 @@ from .physical_params import PhysicalParams, target_delta
 from .simulator import FiniteVoronoiSimulator
 
 
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 
 __all__ = [
     "PhysicalParams",
