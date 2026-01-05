628c958bfdc2d8acd576954c97282994e33810ad
Wei Wang <ww000721@gmail.com>
2026-01-05 00:09:09 -0500

    :art: Ship only the CPP file at build time

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ Created by Wei Wang, 2025.
 """
 
 from setuptools import setup, Extension
-from Cython.Build import cythonize
 import numpy as np
+# from Cython.Build import cythonize
 
 
 ext = Extension(
-    "pyafv.cell_geom",  # extension module name
-    sources=["pyafv/cell_geom.pyx"],
+    "pyafv.cell_geom",                # extension module name
+    sources=["pyafv/cell_geom.cpp"],  # remove Cython dependency at build time by using the pre-generated C++ file
     extra_compile_args=[],
     extra_link_args=[],
     include_dirs=[np.get_include()],
@@ -24,6 +24,9 @@ ext = Extension(
 )
 
 setup(
-    # name="pyafv",  # project name
-    ext_modules=cythonize([ext], language_level="3", build_dir="build"),
+    # name="pyafv",    # project name
+    # ext_modules=cythonize([ext], language_level="3", build_dir="build"),
+    # Remove Cython dependency at build time by generating .cpp file from .pyx beforehand
+    # with (uv run)+`cython -3 --cplus pyafv/cell_geom.pyx -o pyafv/cell_geom.cpp`
+    ext_modules=[ext]  # without cythonize
 )
