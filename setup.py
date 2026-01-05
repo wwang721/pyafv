df1e3bc40c1c87fa91b013e3b9c141020b260a67
Wei Wang <ww000721@gmail.com>
2026-01-04 22:40:44 -0500

    :bug: Remove project name in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Setup script for compiling Cython extension module.
 ===================================================
 Modules to be compiled:
-- cell_geom.pyx
+- pyafv/cell_geom.pyx
 
 ---------------------------------------------------
 Created by Wei Wang, 2025.
@@ -14,16 +14,16 @@ import numpy as np
 
 
 ext = Extension(
-    "pyafv.cell_geom",
+    "pyafv.cell_geom",  # extension module name
     sources=["pyafv/cell_geom.pyx"],
     extra_compile_args=[],
     extra_link_args=[],
+    include_dirs=[np.get_include()],
     define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
     language="c++",
 )
 
 setup(
-    name="cell_geom",
+    # name="pyafv",  # project name
     ext_modules=cythonize([ext], language_level="3", build_dir="build"),
-    include_dirs=[np.get_include()],
 )
